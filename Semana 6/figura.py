9f245d78ea64aa7f24c855f20f58345889836c50
Gabriel <gabolandia156@gmail.com>
2022-04-30 10:49:18 -0500

    Agregamos Semana 6 con Modulos

---
+++ Semana 6/figura.py
@@ -0,0 +1,10 @@
+import math
+
+def area_triangulo(base = 2, altura = 1): #Se le pone un valor por defecto
+   return (base * altura)/2
+
+def area_circulo(radio = 1): #Se le pone un valor por defecto
+   return math.pi * radio ** 2
+
+def area_cuadrado(lado = 1):
+    return lado ** 2
---
+++ Semana 6/semana6-1.py
@@ -0,0 +1,6 @@
+########## Problema 3 destacado #############
+import figura #En este caso usamos import figura para llamar a el archivo figura donde estan nuestras funciones
+
+print(figura.area_triangulo(2,2)) #Invocamos nuestra funcion de area triangulo
+print(figura.area_circulo(2)) #Invocamos nuestra funcion de area circulo
+print(figura.area_cuadrado(2)) #Invocamos nuestra funcion de area cuadrado
